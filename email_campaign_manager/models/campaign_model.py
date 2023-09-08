6530e976f38b600c9d2e173d6d0fa9c5db948124
apurvalal <apurvalal2019@gmail.com>
2023-09-08 19:43:45 +0530

    Added models

--- email_campaign_manager/models/campaign_model.py
+++ email_campaign_manager/models/campaign_model.py
@@ -1,3 +1,14 @@
-class Campaign():
+from django.db import models
+import uuid
+
+class Campaign(models.Model):
+    campaign_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    subject = models.TextField()
+    preview_text = models.TextField()
+    article_url = models.TextField()
+    html_content = models.TextField()
+    plain_text_content = models.TextField()
+    published_date = models.DateTimeField()
+
     def __str__(self):
         return self.subject
--- email_campaign_manager/models/subscriber_model.py
+++ email_campaign_manager/models/subscriber_model.py
@@ -1,3 +1,9 @@
-class Subscriber():
+from django.db import models
+
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.TextField()
+    is_active = models.BooleanField(default=True)
+
     def __str__(self):
-        return self.subject
+        return self.email
