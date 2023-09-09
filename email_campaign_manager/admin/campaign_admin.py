d8da6d92606f953ff30a10f824915a731193c556
apurvalal <apurvalal2019@gmail.com>
2023-09-09 16:21:40 +0530

    Added campaign id in admin interface

--- email_campaign_manager/admin/campaign_admin.py
+++ email_campaign_manager/admin/campaign_admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from email_campaign_manager.models.campaign_model import Campaign
 
-admin.site.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'campaign_id', 'published_date')
+
+admin.site.register(Campaign, CampaignAdmin)
