62af835aa9b92da92c11778aabbd8ecb5a8fcd89
apurvalal <apurvalal2019@gmail.com>
2023-09-09 14:17:06 +0530

    Added unsubscribe and add subscriber endpoints

--- email_campaign_manager/controllers/subscriber_controller.py
+++ email_campaign_manager/controllers/subscriber_controller.py
@@ -1,7 +1,17 @@
-def add_subscriber():
-    # todo: add function
-    return
+from django.http import JsonResponse
+from email_campaign_manager.models.subscriber_model import Subscriber
 
-def unsubscribe():
-    # todo: add function
-    return
+def add_subscriber(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        Subscriber.objects.create(email=email, first_name=first_name, is_active=True)
+        return JsonResponse({"status_code": 200, "message": "Subscriber successfully created"})
+
+def unsubscribe(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        subscriber = Subscriber.objects.get(email=email)
+        subscriber.is_active = False
+        subscriber.save()
+        return JsonResponse({"status_code": 200, "message": "Unsubscribed successfully"})
